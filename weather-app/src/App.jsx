241f70cfc2dcb36ed689ca1cc0c04f649a904618
Kristoffer Hansson <97549161+Zarser@users.noreply.github.com>
2024-11-13 10:13:52 +0100

    update

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import WeatherCard from "./Components/weatherCard";
 import './styles/weatherCard.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="center-content">
- 
+       < WeatherCard />
     </div>
     
   );
